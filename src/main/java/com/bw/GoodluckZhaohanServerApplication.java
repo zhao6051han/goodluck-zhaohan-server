00bd7704bf1a36ea759717bdf4e641b7f41b095a
zhaohan <694676765@qq.com>
2017-07-28 18:39:11 +0800

    Initial commit

---
+++ src/main/java/com/bw/GoodluckZhaohanServerApplication.java
@@ -0,0 +1,12 @@
+package com.bw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodluckZhaohanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodluckZhaohanServerApplication.class, args);
+	}
+}
